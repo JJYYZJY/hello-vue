0830197c4fc28ab35d458c84279a3bfe4679f8d3
zhangjy02 <zhangjy02@fenbi.com>
2020-04-17 22:27:24 +0800

    ADD:增加抽屉设置布局

---
+++ src/components/SettingDrawer.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <a-drawer
+      title="Basic Drawer"
+      placement="right"
+      :closable="false"
+      @close="onClose"
+      :visible="visible"
+      :afterVisibleChange="afterVisibleChange"
+      width="300px"
+    >
+      <template v-slot:handle>
+        <div class="handle" @click="visible = !visible">
+          <a-icon :type="visible ? 'close' : 'setting'"></a-icon>
+        </div>
+      </template>
+      <div>
+        <div>
+          <h2>主题</h2>
+          <a-radio-group v-model="navTheme">
+            <a-radio value="dark">黑色</a-radio>
+            <a-radio value="light">白色</a-radio>
+          </a-radio-group>
+          <h2>菜单</h2>
+          <a-radio-group v-model="navLayout">
+            <a-radio value="left">左侧</a-radio>
+            <a-radio value="top">顶部</a-radio>
+          </a-radio-group>
+        </div>
+      </div>
+    </a-drawer>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      visible: false,
+      navTheme: "dark",
+      navLayout: "left"
+    };
+  },
+  methods: {
+    afterVisibleChange(val) {
+      console.log("visible", val);
+    },
+    onClose() {
+      this.visible = false;
+    }
+  }
+};
+</script>
+<style scope>
+.handle {
+  position: absolute;
+  top: 240px;
+  right: 300px;
+  background: rgb(41, 180, 235);
+  height: 48px;
+  width: 48px;
+  line-height: 48px;
+  font-size: 20px;
+  text-align: center;
+  color: white;
+  border-radius: 8px 0 0 8px;
+}
+</style>
--- src/layout/BasicLayout.vue
+++ src/layout/BasicLayout.vue
@@ -1,31 +1,35 @@
 <template>
-  <a-layout id="components-layout-demo-side" style="min-height: 100vh">
-    <a-layout-sider :trigger="null" collapsible v-model="collapsed">
-      <SiderMenu />
-    </a-layout-sider>
-    <a-layout>
-      <a-layout-header style="background: #fff; padding: 0">
-        <a-icon
-          class="trigger"
-          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
-          @click="collapsed = !collapsed"
-        />
-        <Header />
-      </a-layout-header>
-      <a-layout-content style="margin: 0 16px">
-        <router-view></router-view>
-      </a-layout-content>
-      <a-layout-footer style="text-align: center">
-        <Footer />
-      </a-layout-footer>
+  <div>
+    <a-layout id="components-layout-demo-side" style="min-height: 100vh">
+      <a-layout-sider :trigger="null" collapsible v-model="collapsed">
+        <SiderMenu />
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header style="background: #fff; padding: 0">
+          <a-icon
+            class="trigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="collapsed = !collapsed"
+          />
+          <Header />
+        </a-layout-header>
+        <a-layout-content style="margin: 0 16px">
+          <router-view></router-view>
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">
+          <Footer />
+        </a-layout-footer>
+      </a-layout>
     </a-layout>
-  </a-layout>
+    <SettingDrawer />
+  </div>
 </template>
 
 <script>
 import Header from "./Header";
 import Footer from "./Footer";
 import SiderMenu from "./SiderMenu";
+import SettingDrawer from "../components/SettingDrawer";
 export default {
   data() {
     return {
@@ -35,7 +39,8 @@ export default {
   components: {
     Header,
     Footer,
-    SiderMenu
+    SiderMenu,
+    SettingDrawer
   }
 };
 </script>
