bdcd4c4c9c2b4338319be7fadd34944f5df41851
zhangjy02 <zhangjy02@fenbi.com>
2020-04-19 20:48:08 +0800

    ADD:增加菜单

--- src/layout/BasicLayout.vue
+++ src/layout/BasicLayout.vue
@@ -7,9 +7,10 @@
         :trigger="null"
         collapsible
         v-model="collapsed"
+        width="256px"
       >
         <div class="logo">ant desgin vue pro</div>
-        <SiderMenu />
+        <SiderMenu :theme="navTheme" />
       </a-layout-sider>
       <a-layout>
         <a-layout-header style="background: #fff; padding: 0">
