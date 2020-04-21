c28cd461c60a97fb5adbd84a184cda268ee7481d
zhangjy02 <zhangjy02@fenbi.com>
2020-04-21 19:05:00 +0800

    MOD:抽取成通用图表组件

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="chartDom" style="height: 400px"></div>
+  <div ref="chartDom"></div>
 </template>
 
 <script>
@@ -7,37 +7,18 @@ import echarts from "echarts";
 import { addListener, removeListener } from "resize-detector";
 import debounce from "lodash/debounce";
 export default {
+  props: {
+    option: {
+      type: Object,
+      default: () => {}
+    }
+  },
   created() {
     this.resize = debounce(this.resize, 300);
   },
   mounted() {
-    // 基于准备好的dom，初始化echarts实例
-    this.chart = echarts.init(this.$refs.chartDom);
+    this.renderChart();
     addListener(this.$refs.chartDom, this.resize);
-    // 指定图表的配置项和数据
-    var option = {
-      title: {
-        text: "ECharts 入门示例"
-      },
-      tooltip: {},
-      legend: {
-        data: ["销量"]
-      },
-      xAxis: {
-        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-      },
-      yAxis: {},
-      series: [
-        {
-          name: "销量",
-          type: "bar",
-          data: [5, 20, 36, 10, 10, 20]
-        }
-      ]
-    };
-
-    // 使用刚指定的配置项和数据显示图表。
-    this.chart.setOption(option);
   },
   beforeDestroy() {
     removeListener(this.$refs.chartDom, this.resize);
@@ -47,6 +28,12 @@ export default {
   methods: {
     resize() {
       this.chart.resize();
+    },
+    renderChart() {
+      // 基于准备好的dom，初始化echarts实例
+      this.chart = echarts.init(this.$refs.chartDom);
+      // 使用刚指定的配置项和数据显示图表。
+      this.chart.setOption(this.option);
     }
   }
 };
--- src/views/Page2.vue
+++ src/views/Page2.vue
@@ -2,13 +2,38 @@
   <div>
     page2
     <br />
-    <Chart />
+    <Chart style="height: 400px" :option="chartOption" />
   </div>
 </template>
 
 <script>
 import Chart from "../components/Chart";
 export default {
+  data() {
+    return {
+      // 指定图表的配置项和数据
+      chartOption: {
+        title: {
+          text: "ECharts 入门示例"
+        },
+        tooltip: {},
+        legend: {
+          data: ["销量"]
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      }
+    };
+  },
   components: {
     Chart
   }
