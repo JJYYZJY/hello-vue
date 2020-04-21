da130fad9fb92b9d35ed27daa20fd86723e81994
zhangjy02 <zhangjy02@fenbi.com>
2020-04-21 22:13:25 +0800

    ADD:封装网络请求工具类

---
+++ src/utils/request.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { notification } from "ant-design-vue";
+
+function request(options) {
+  return axios(options)
+    .then(res => {
+      return res;
+    })
+    .catch(error => {
+      const {
+        response: { status, statusText }
+      } = error;
+      notification.error({
+        message: status,
+        description: statusText
+      });
+      return Promise.reject(error);
+    });
+}
+
+export default request;
--- src/views/Page2.vue
+++ src/views/Page2.vue
@@ -9,7 +9,8 @@
 <script>
 import Chart from "../components/Chart";
 // import random from "lodash/random";
-import axios from "axios";
+// import axios from "axios";
+import request from "../utils/request";
 export default {
   data() {
     return {
@@ -35,30 +36,32 @@ export default {
   },
   methods: {
     getChartData() {
-      axios
-        .get("/api/dashboard/chart", { params: { id: 8 } })
-        .then(response => {
-          this.chartOption = {
-            title: {
-              text: "ECharts 入门示例"
-            },
-            tooltip: {},
-            legend: {
-              data: ["销量"]
-            },
-            xAxis: {
-              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-            },
-            yAxis: {},
-            series: [
-              {
-                name: "销量",
-                type: "bar",
-                data: response.data
-              }
-            ]
-          };
-        });
+      request({
+        url: "/api/dashboard/chart",
+        method: "get",
+        params: { id: 8 }
+      }).then(response => {
+        this.chartOption = {
+          title: {
+            text: "ECharts 入门示例"
+          },
+          tooltip: {},
+          legend: {
+            data: ["销量"]
+          },
+          xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+          },
+          yAxis: {},
+          series: [
+            {
+              name: "销量",
+              type: "bar",
+              data: response.data
+            }
+          ]
+        };
+      });
     }
   }
 };
