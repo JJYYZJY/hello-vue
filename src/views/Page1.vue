468add9883310ed15911e839c76ae05003ada9db
zhangjy02 <zhangjy02@fenbi.com>
2020-04-21 22:56:04 +0800

    ADD:加载svg图

--- src/views/Page1.vue
+++ src/views/Page1.vue
@@ -3,14 +3,17 @@
     page1 {{ msg }}
     <br />
     <a-button v-auth="['admin']">button</a-button>
+    <img :src="logo" />
   </div>
 </template>
 
 <script>
+import logo from "@/assets/home.svg";
 export default {
   data() {
     return {
-      msg: "test"
+      msg: "test",
+      logo: logo
     };
   }
 };
