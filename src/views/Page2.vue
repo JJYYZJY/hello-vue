efd6986a045a246ba7cc7cf2d64f6c4ce73dedc4
zhangjy02 <zhangjy02@fenbi.com>
2020-04-21 21:22:10 +0800

    ADD:mock 图表数据

--- src/views/Page2.vue
+++ src/views/Page2.vue
@@ -8,46 +8,58 @@
 
 <script>
 import Chart from "../components/Chart";
-import random from "lodash/random";
+// import random from "lodash/random";
+import axios from "axios";
 export default {
   data() {
     return {
       // 指定图表的配置项和数据
-      chartOption: {
-        title: {
-          text: "ECharts 入门示例"
-        },
-        tooltip: {},
-        legend: {
-          data: ["销量"]
-        },
-        xAxis: {
-          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-        },
-        yAxis: {},
-        series: [
-          {
-            name: "销量",
-            type: "bar",
-            data: [5, 20, 36, 10, 10, 20]
-          }
-        ]
-      }
+      chartOption: {}
     };
   },
   components: {
     Chart
   },
   mounted() {
+    this.getChartData();
     this.interval = setInterval(() => {
-      this.chartOption.series[0].data = this.chartOption.series[0].data.map(
-        () => random(100)
-      );
-      this.chartOption = { ...this.chartOption };
+      this.getChartData();
+      // this.chartOption.series[0].data = this.chartOption.series[0].data.map(
+      //   () => random(100)
+      // );
+      // this.chartOption = { ...this.chartOption };
     }, 3000);
   },
   beforeDestroy() {
     clearInterval(this.interval);
+  },
+  methods: {
+    getChartData() {
+      axios
+        .get("/api/dashboard/chart", { params: { id: 8 } })
+        .then(response => {
+          this.chartOption = {
+            title: {
+              text: "ECharts 入门示例"
+            },
+            tooltip: {},
+            legend: {
+              data: ["销量"]
+            },
+            xAxis: {
+              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [
+              {
+                name: "销量",
+                type: "bar",
+                data: response.data
+              }
+            ]
+          };
+        });
+    }
   }
 };
 </script>
